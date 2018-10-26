1efae49948e51b6d8cb5c1b8b010942ccc051397
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-26 22:09:59 +0300

    commit solution of 4th task

--- 02. DB APPS INTRODUCTION/src/main/java/aplication/Engine.java
+++ 02. DB APPS INTRODUCTION/src/main/java/aplication/Engine.java
@@ -3,6 +3,7 @@ package aplication;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.BufferOverflowException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -22,7 +23,7 @@ public class Engine implements Runnable {
     public void run() {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            this.getVillainNameAndMinions(Integer.parseInt(reader.readLine()));
+            this.addMinion();
         } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
@@ -61,10 +62,11 @@ public class Engine implements Runnable {
     private void getVillainNameAndMinions(int villainId) throws SQLException {
 
 
-        String query = String.format("SELECT v.name, m.name, m.age FROM minions m JOIN minions_villains m2 ON m.id = m2.minion_id RIGHT JOIN villains v ON m2.villain_id = v.id WHERE v.id = %d", villainId);
+        String query = String.format("SELECT v.name, m.name, m.age FROM minions m JOIN minions_villains m2 ON m.id = " +
+                "m2.minion_id RIGHT JOIN villains v ON m2.villain_id = v.id WHERE v.id = %d", villainId);
         PreparedStatement preparedStatement = this.connection.prepareStatement(query);
         ResultSet resultSet = preparedStatement.executeQuery();
-        if (!resultSet.first()) {
+        if (!resultSet.next()) {
             System.out.printf("No villain with ID %d exists in the database.", villainId);
         } else if (resultSet.getString(2) == null) {
             System.out.printf("Villain: %s", resultSet.getString(1));
@@ -76,6 +78,80 @@ public class Engine implements Runnable {
                 index++;
             } while (resultSet.next());
         }
+    }
+
+
+    /**
+     * 4.	Add Minion
+     * Write a program that reads information about a minion and its villain and adds it to the database.
+     * In case the town of the minion is not in the database, insert it as well.
+     * In case the villain is not present in the database, add him too with default evilness factor of “evil”.
+     * Finally, set the new minion to be servant of the villain.
+     * Print appropriate messages after each operation – see the examples.
+     */
+
+    private void addMinion() throws IOException, SQLException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] minionData = reader.readLine().split("\\s+");
+        String[] villainData = reader.readLine().split("\\s+");
+
+        String minionName = minionData[1];
+        int minionAge = Integer.parseInt(minionData[2]);
+        String town = minionData[3];
+
+        String villainName = villainData[1];
+
+        if(!checkIfExistTown(town)){
+            String query = String.format("INSERT INTO towns(name, country) VALUES('%s',NULL)",town);
+            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.executeUpdate();
+            System.out.printf("Town %s was added to the database.%n",town);
+        }
+        if (!checkIfExistVillain(villainName)){
+            String query = String.format("INSERT INTO villains(name, evilness_factor) VALUES('%s','evil')",villainName);
+            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.executeUpdate();
+            System.out.printf("Villain %s was added to the database.%n",villainName);
+        }
+        int townId = this.checkId(town,"towns");
+
+        String query = String.format("INSERT INTO minions(name, age, town_id) VALUES('%s',%d,%d)",minionName,minionAge,townId);
+        PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+        preparedStatement.executeUpdate();
+
+        int minionID = this.checkId(minionName, "minions");
+        int villainID = this.checkId(villainName, "villains");
+        String query1 = String.format("INSERT INTO minions_villains(minion_id,villain_id) VALUES(%d,%d)", minionID, villainID);
+        PreparedStatement ps = this.connection.prepareStatement(query1);
+        ps.executeUpdate();
+        System.out.printf("Successfully added %s to be minion of %s%n",minionName, villainName);
+    }
+
+    public boolean checkIfExistTown(String columnName) throws SQLException {
+        String query = String.format("SELECT * FROM towns WHERE name = '%s'", columnName);
+        PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (!resultSet.next()){
+            return false;
+        }
+        return true;
+    }
+    public boolean checkIfExistVillain(String villainName) throws SQLException {
+        String query = String.format("SELECT * FROM villains WHERE name = '%s'", villainName);
+        PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (!resultSet.next()){
+            return false;
+        }
+        return true;
+    }
+    public int checkId(String name, String tableName) throws SQLException {
+        String query = String.format("SELECT t.id FROM %s t WHERE t.name = '%s'", tableName, name);
+        PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        return resultSet.getInt(1);
     }
 }
 
