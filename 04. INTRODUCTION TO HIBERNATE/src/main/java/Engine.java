40f88e1cff8d31aa44aa9d3218139153fe4d1d2e
Stefan Stoynov <sstoynov87@gmail.com>
2018-11-03 09:21:34 +0200

    commit solution of 4th task

--- 04. INTRODUCTION TO HIBERNATE/src/main/java/Engine.java
+++ 04. INTRODUCTION TO HIBERNATE/src/main/java/Engine.java
@@ -1,10 +1,14 @@
 
 
 
-
+import entities.Employee;
 import entities.Town;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 
@@ -16,7 +20,11 @@ public class Engine implements Runnable {
     }
 
     public void run() {
-        this.removeObject();
+        try {
+            this.containsEmployee();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -24,12 +32,36 @@ public class Engine implements Runnable {
      * Use the soft_uni database. Persist all towns from the database. Detach those whose name length is more than
      * 5 symbols. Then transform the names of all attached towns to lowercase and save them to the database.
      */
-    private void removeObject(){
+    private void removeObject() {
         this.entityManager.getTransaction().begin();
         List<Town> townsDetached = this.entityManager.createNativeQuery
                 ("SELECT * FROM towns t WHERE char_length(t.name) > 5", Town.class).getResultList();
-        townsDetached.forEach(t-> t.setName(t.getName().toLowerCase()));
+        townsDetached.forEach(t -> t.setName(t.getName().toLowerCase()));
         this.entityManager.getTransaction().commit();
+    }
+
+    /**
+     * 3.	Contains Employee
+     * Use the soft_uni database. Write a program that checks if a given employee name is contained in the database.
+     */
+
+    private void containsEmployee() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String inputName = reader.readLine();
+
+        this.entityManager.getTransaction().begin();
+        try {
+            Employee employee = this.entityManager
+                    .createQuery("FROM Employee WHERE concat(first_name, ' ', last_name) = :name", Employee.class)
+                    .setParameter("name", inputName)
+                    .getSingleResult();
+
+            System.out.println("Yes");
+        }catch (NoResultException nre){
+            System.out.println("No");
+        }
+
     }
 
 }
